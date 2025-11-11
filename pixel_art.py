14076eecb768c849305ade3adb8ab1086c2d5c44
EthanT-E <trippedwardse@gmail.com>
2025-11-11 14:53:20 +0000

    changed bee colour

--- pixel_art.py
+++ pixel_art.py
@@ -7,10 +7,10 @@ def define_bee():
     # define some colours
     black = [0, 0, 0]
     yellow = [1.0, 0.85, 0]
-    grey = [0.75] * 3
+    grey = [0.44] * 3
 
     # specify which pixels are which colour
-    image_mat[7:11, 2] = grey
+    image_mat[7:11, 2] = black
     image_mat[6:12, 3:5] = black
     image_mat[6:12, 5:7] = yellow
     image_mat[6:12, 7:9] = black
